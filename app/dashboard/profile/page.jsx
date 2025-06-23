24b312e892fe8873a2861415d94058aa339a4468
unknown <randalchukzwilson@gmail.com>
2025-06-23 12:30:21 +0100

    first commit for Writer

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -1,10 +1,16 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/auth"
 import UserProfilePage from "./ProfilePage"
+import { Suspense } from "react"
+import Loading from "@/app/loading"
 
 export default async function Page() {
   const session = await getServerSession(authOptions)
   const user = session?.user
 
-  return <UserProfilePage user={user} />
+  return (
+    <Suspense fallback={<Loading />}>
+      <UserProfilePage user={user} />
+    </Suspense>
+  )
 }
